4f757580eea46c9acc64e5c40348f5ede6850102
rajshubhamkashyap2020 <157522814+rajshubhamkashyap2020@users.noreply.github.com>
2024-06-18 18:31:25 +0530

    New Module for Address Service

--- employeeService/src/main/java/ms/api/controller/EmployeeController.java
+++ employeeService/src/main/java/ms/api/controller/EmployeeController.java
@@ -4,12 +4,13 @@ package ms.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,13 +18,19 @@ import ms.api.entity.Employee;
 import ms.api.service.EmployeeService;
 
 @RestController
-@RequestMapping("/Employees")
 public class EmployeeController {
 	
 	@Autowired
     private EmployeeService employeeService;
+	
+	
+	
+	@GetMapping("/")
+    public String home(@AuthenticationPrincipal OAuth2User principal) {
+        return "Welcome, " + principal.getAttribute("login");
+    }
 
-	@GetMapping("/getAllEmployee")
+	@GetMapping("/Employees")
 	public List<Employee> getAllEmployee() {
 		List<Employee>empList=employeeService.getAllEmployee();
 		
@@ -31,7 +38,7 @@ public class EmployeeController {
 		
 	}
 	
-	@DeleteMapping("/delete/{eid}")
+	@DeleteMapping("/Employees/delete/{eid}")
 	public String deleteEmployeeById(@PathVariable int eid) {
 		try {
 			
@@ -43,12 +50,12 @@ public class EmployeeController {
 		}	
 	}
 	
-    @PostMapping
+    @PostMapping("/Employees/create")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.createEmployee(employee);
     }
     
-    @PostMapping("/update")
+    @PostMapping("/Employees/update")
     public String updateEmployee(@RequestBody Employee employee) throws Exception {
     	employeeService.updateEmployee(employee);
     	
